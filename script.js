57371dc4945b74b5dde28d36ce78bd113176c1e4
ALİ CANBOLAT <alicnblt1604@gmail.com>
2023-02-25 17:29:47 +0300

    script commit

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ imageModal.addEventListener("click", (e) => {
     imageModal.classList.remove("toggled");
   }
 });
+
+
+// const container = document.querySelector('.col-images');
+// const unplashImage = 'https://source.unsplash.com/random/';
+// const rows = 5;
+
+// for (let i = 0; i < rows * 3; i++) {
+//     const img = document.createElement('img');
+//     img.src = '${unplashImage}${getRandomSize()}';
+//     container.appendChild(img);
+    
+// }
+
+// function getRandomSize(){
+//     return '${getRandomNr()}x${getRandomNr()}'
+// }
+
+// function getRandomNr(){
+//     return Math.floor(Math.random() * 10)+300;
+// }
